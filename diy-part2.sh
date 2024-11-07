b249dffbae9a12d5a64a3c562bb19cd988431182
zhuzhu888666 <167627499+zhuzhu888666@users.noreply.github.com>
2024-11-08 03:19:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,6 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/Zxilly/UA2F.git package/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
